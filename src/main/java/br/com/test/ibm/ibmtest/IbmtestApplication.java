9bb98600251fa64c55b39c6256391900e3d24c2b
bprates <bprates.ti@gmail.com>
2020-07-10 19:24:53 -0300

    teste

---
+++ src/main/java/br/com/test/ibm/ibmtest/IbmtestApplication.java
@@ -0,0 +1,13 @@
+package br.com.test.ibm.ibmtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbmtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/test/ibm/ibmtest/IbmtestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.test.ibm.ibmtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbmtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
